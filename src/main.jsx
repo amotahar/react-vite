83b126de691b130fd1a7dd5d032db019c248e519
atuhin <atuhinbd@gmail.com>
2023-01-18 14:24:05 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React, { Children } from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from './routes/root';
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+import { loader as rootLoader } from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    Children: [
+    {
+      path: "contacts/:contactId",
+      element: <Contact />,
+    },
+  ],
+},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
